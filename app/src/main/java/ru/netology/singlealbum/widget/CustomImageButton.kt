2abb106b6430eb15a751746a9203a9121730878e
Sergei Bykov <ss.bykoff@gmail.com>
2024-07-23 23:24:21 +0300

    расширил костомизация кнопки, параметризовал иконки

--- app/src/main/java/ru/netology/singlealbum/widget/CustomImageButton.kt
+++ app/src/main/java/ru/netology/singlealbum/widget/CustomImageButton.kt
@@ -1,23 +1,24 @@
 package ru.netology.singlealbum.widget
 
 import android.content.Context
+import android.graphics.drawable.Drawable
 import android.util.AttributeSet
 import androidx.appcompat.widget.AppCompatImageButton
+import androidx.core.content.ContextCompat
 import ru.netology.singlealbum.R
 
-class CustomImageButton : AppCompatImageButton {
+class CustomImageButton @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = androidx.appcompat.R.attr.imageButtonStyle
+) : AppCompatImageButton(context, attrs, defStyleAttr) {
     private var isChecked = false
     private var onCheckedChangeListener: ((Boolean) -> Unit)? = null
-
-    constructor(context: Context) : super(context)
-    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs)
-    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(
-        context,
-        attrs,
-        defStyleAttr
-    )
+    private var normalIcon = ContextCompat.getDrawable(context, R.drawable.ic_play_arrow_24)
+    private var pressedIcon = ContextCompat.getDrawable(context, R.drawable.ic_pause_24)
 
     init {
+
         setOnClickListener {
             isChecked = !isChecked
             updateButtonState()
@@ -25,11 +26,14 @@ class CustomImageButton : AppCompatImageButton {
         }
     }
 
+    fun setIcons(normalIcon: Int, pressedIcon: Int) {
+        this.normalIcon = ContextCompat.getDrawable(context, normalIcon)
+        this.pressedIcon = ContextCompat.getDrawable(context, pressedIcon)
+    }
+
     private fun updateButtonState() {
         isPressed = isChecked
-        setImageResource(
-            if (isChecked) R.drawable.ic_pause_24 else R.drawable.ic_play_arrow_24
-        )
+        setImageDrawable(if (isPressed) pressedIcon else normalIcon)
     }
 
     fun setChecked(checked: Boolean) {
